b3ff63eb5b706a9d081b0ca8668696694cb45735
KristinaShepeleva <shepelevakristina52@gmail.com>
2023-02-18 20:47:01 +0200

    5-fix

--- js/task-05.js
+++ js/task-05.js
@@ -4,5 +4,8 @@ const titleRef = document.querySelector('#name-output');
 
 inputRef.addEventListener('input', (event) => {
     titleRef.textContent = event.currentTarget.value; 
+    if (event.currentTarget.value === '') {
+        titleRef.textContent = 'Anonymous';
+    }
 });
 
