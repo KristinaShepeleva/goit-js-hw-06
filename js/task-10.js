d31838757ee10489fda87958075a4092a10afb6a
KristinaShepeleva <shepelevakristina52@gmail.com>
2023-02-21 21:59:45 +0200

    10-task

--- js/task-10.js
+++ js/task-10.js
@@ -1,31 +1,44 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
-
-
-
-//Напиши скрипт створення і очищення колекції елементів. Користувач вводить кількість елементів в input і натискає кнопку Створити, після чого рендериться колекція. Натисненням на кнопку Очистити, колекція елементів очищається.
-
-//<div id="controls">
-//  <input type="number" min="1" max="100" step="1" />
-//  <button type="button" data-create>Create</button>
-//  <button type="button" data-destroy>Destroy</button>
-//</div>
 
 const controlsRef = document.querySelector("#controls");
+const inputRef = document.querySelector("input");
+const buttonCreateRef = document.querySelector("button[data-create]");
+const buttonDestroyRef = document.querySelector("button[data-destroy]");
+const boxRef = document.querySelector("#boxes");
 
+let amount = Number(inputRef.getAttribute("min"));
+console.log(amount)
 
-//<div id="boxes"></div>
+buttonCreateRef.addEventListener('click', (ev) => {
+   createBoxes(amount)
+});
 
-//Створи функцію createBoxes(amount), яка приймає один параметр - число. Функція створює стільки <div>, скільки вказано в amount і додає їх у div#boxes.
+inputRef.addEventListener('input', (event) => {
+  amount = Number(event.currentTarget.value);  
+});
 
-//Розміри найпершого <div> - 30px на 30px.
-//Кожен елемент після першого повинен бути ширшим і вищим від попереднього на 10px.
-//Всі елементи повинні мати випадковий колір фону у форматі HEX. Використовуй готову функцію getRandomHexColor для отримання кольору.
-//function getRandomHexColor() {
-//  return `#${Math.floor(Math.random() * 16777215)
-//    .toString(16)
-//    .padStart(6, 0)}`;
-//}
+buttonDestroyRef.addEventListener('click', destroyBoxes);
 
-//Створи функцію destroyBoxes(), яка очищає вміст div#boxes, у такий спосіб видаляючи всі створені елементи.
+let createBoxs = [];
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i += 1) {
+    const box = document.createElement('div');
+    const firstBoxSize = 30;
+    const size = firstBoxSize + i * 10;
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    createBoxs.push(box);
+    console.log(createBoxs);
+  }
+  boxRef.append(...createBoxs);
+}
+ 
+function destroyBoxes() {
+  boxRef.innerHTML = '';
+  createBoxs.splice(0, createBoxs.length);
+}
+
+ function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
